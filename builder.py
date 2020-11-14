a89b7c5aecdeb0211f6836614e6ff91f5ce5a1a8
AAAA <dev@onerbs.com>
2020-11-14 15:23:18 -0600

    Add `vlang_builder` command

---
+++ builder.py
@@ -0,0 +1,12 @@
+import sublime, sublime_plugin
+
+class VlangBuilderCommand(sublime_plugin.WindowCommand):
+    def parse(self, **kwargs):
+        if kwargs['shell_cmd']: return kwargs
+        file = self.window.active_view().file_name().split('/')[-1]
+        flag = 'test' if '_test.v' in file else 'run'
+        kwargs['shell_cmd'] = 'v %s %s' % (flag, file)
+        return kwargs
+
+    def run(self, **kwargs):
+        self.window.run_command('exec', self.parse(**kwargs))
