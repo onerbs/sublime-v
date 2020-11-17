3f5565353467b9229bbaf1f69788a094c474f6a4
AAAA <dev@onerbs.com>
2020-11-17 04:33:55 -0600

    Fix `KeyError`

--- builder.py
+++ builder.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin
 
 class VlangBuilderCommand(sublime_plugin.WindowCommand):
     def parse(self, **kwargs):
-        if kwargs['shell_cmd']: return kwargs
+        if kwargs.get('shell_cmd'): return kwargs
         file = self.window.active_view().file_name().split('/')[-1]
         flag = 'test' if '_test.v' in file else 'run'
         kwargs['shell_cmd'] = 'v %s %s' % (flag, file)
